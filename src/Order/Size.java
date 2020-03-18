44e8206316aabc22b983c15dc0b5439ddaa3b45c
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-19 00:34:21 +0700

    Create Size.java

---
+++ src/Order/Size.java
@@ -0,0 +1,23 @@
+package Order;
+
+
+public class Size {
+	String name;
+	String itemID;
+
+	public String getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String string) {
+		this.name = string;
+	}
+}
