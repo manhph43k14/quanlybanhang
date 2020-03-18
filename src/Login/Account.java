196b6d550e5faf71428600db6fdbb3464ca19ed0
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-19 00:34:06 +0700

    Create Account.java

---
+++ src/Login/Account.java
@@ -0,0 +1,29 @@
+package Login;
+
+public class Account {
+	String username, password, id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String position) {
+		this.id = position;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
